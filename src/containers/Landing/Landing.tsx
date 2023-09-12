05efb8103aa35ab5b2e389558f441b08fbfcf250
Jamie Rossiter <jamieprossiter@gmail.com>
2023-09-12 14:02:53 +1000

    Small changes to layout/design

--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -7,7 +7,7 @@ export const Landing = () => {
             <div className={styles.parent}>
                 <div className={styles.container}>
                     <img src="./img/beja_logo.png" className={styles.logo} />
-                    <p className={styles.text}>Melbourne-based web design and branding duo. SquareSpace experts.</p>
+                    <p className={styles.text}>👋 We’re a Melbourne-based SquareSpace web design duo, committed to developing sleek and user-friendly websites tailored to your business.</p>
                     <div className={styles.buttonContainer}>
                         <Button text="Enquire Now" />
                     </div>
